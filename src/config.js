62b77f52912b2362f692da8143dbf470377514f5
saeemo <saeedz.1424@gmail.com>
2026-06-27 22:02:31 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,27 +1,17 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'saeedmozahrani@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/saeemo',
     },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
-    },
+    
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/saeemo',
     },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
+   
   ],
 
   navLinks: [
